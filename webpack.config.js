df862feaa7a34e4284e4081c1dce4d57fb8f2601
Vidya Sagar <vidyasagar.vi@gmail.com>
2024-06-14 09:58:52 +0530

    Add webpack for build

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // Entry point of your application
+    output: {
+        filename: 'bundle.js', // Output file name
+        path: path.resolve(__dirname, 'dist') // Output directory
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Apply this rule to all .js files
+                exclude: /node_modules/, // Exclude node_modules
+                use: {
+                    loader: 'babel-loader', // Use Babel loader to transpile JavaScript
+                    options: {
+                        presets: ['@babel/preset-env'] // Use preset-env for ES6+ features
+                    }
+                }
+            }
+        ]
+    }
+};
