39b7c23db62d0725220da54910187ee192162a3f
Vidya Sagar <vidyasagar.vi@gmail.com>
2024-06-14 10:58:07 +0530

    update compatibilty for ES6

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import path from 'path';
 
-module.exports = {
-    entry: './src/index.js', // Entry point of your application
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    entry: './src/index.js',
     output: {
-        filename: 'bundle.js', // Output file name
-        path: path.resolve(__dirname, 'dist') // Output directory
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
-                test: /\.js$/, // Apply this rule to all .js files
-                exclude: /node_modules/, // Exclude node_modules
+                test: /\.js$/,
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader', // Use Babel loader to transpile JavaScript
-                    options: {
-                        presets: ['@babel/preset-env'] // Use preset-env for ES6+ features
-                    }
+                    loader: 'babel-loader'
                 }
             }
         ]
